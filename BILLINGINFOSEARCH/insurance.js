62a743e320bda1d77e8e5571b5141250674b53d5
EdwinSmart <SMART3409@GMAIL.COM>
2024-05-30 16:42:12 +0300

    Add files via upload

---
+++ BILLINGINFOSEARCH/insurance.js
@@ -0,0 +1,317 @@
+const insuranceCompaniesData = [
+  {
+    "name": "UAP Insurance",
+    "phoneNumber": "0711065100",
+    "email": "Omoutpatient@oldmutual.co.ke",
+    "outpatientLimit": "Kshs 10,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      }
+    ],
+    "icon": "uap.jpg" 
+  },
+  {
+    "name": "MADISON Insurance",
+    "phoneNumber": "0709922555/0202864555",
+    "email": "caremanagement@madison.co.ke",
+    "outpatientLimit": "Kshs  10,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "Madison.jpg" 
+  },
+{
+    "name": "JUBILEE Insurance",
+    "phoneNumber": "0709949000/0203281000",
+    "email": "medicalcaremanagers@jubilee.co.ke",
+    "outpatientLimit": "Kshs  15,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "Jubilee.jpg" 
+  },
+  {
+    "name": "CIC Insurance",
+    "phoneNumber": "0703099999/0203823999",
+    "email": "Outpatientapprovals@cic.co.ke",
+    "outpatientLimit": "Kshs  7,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "Cic2.jpeg" 
+  },
+ {
+    "name": "HERITAGE Insurance",
+    "phoneNumber": "0728111002/0728607689/0728111001/0733750004/0703550050",
+    "email": "Healthcareundertakings@heritage.co.ke",
+    "outpatientLimit": "Kshs  15,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "Heritage.jpg" 
+  },
+{
+    "name": "APA Insurance",
+    "phoneNumber": "(0709912777, (020) 286 2000 , (020) 364 1000",
+    "email": "Care.team@apa.co.ke",
+    "outpatientLimit": "Kshs  10,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "APA.jpg" 
+  },
+{
+    "name": "AAR Insurance",
+    "phoneNumber": "0730633000/0703063000",
+    "email": "aarcallcenter@mtiba.co.ke",
+    "outpatientLimit": "Kshs  10,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "AAR.jpg" 
+  },
+{
+    "name": "GA Insurance",
+    "phoneNumber": "0709626400",
+    "email": "careteam@gakenya.com",
+    "outpatientLimit": "Kshs  10,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "GA.jpg" 
+  },
+{
+    "name": "CIGNA Insurance",
+    "phoneNumber": "INTERNATIONAL",
+    "email": "bills@cigna.com/call@cigna.com",
+    "outpatientLimit": "USD 400",
+    "preAuthorizationServices": ["CHECK CIGNA PORTAL"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "Cigna.jpg" 
+  },
+{
+    "name": "PACIS Insurance",
+    "phoneNumber": "0720113122/0733777717",
+    "email": "Medical@paciskenya.com/Casemanagement@paciskenya.com",
+    "outpatientLimit": "10,000",
+    "billingSystem": "Smart",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "Pacis.jpg" 
+  },
+{
+    "name": "EAGLE AFRICA Insurance",
+    "phoneNumber": "0722232246/0722201238/0736363959",
+    "email": "eagleafrica@eagleafrica.co.ke/eagleafrica.msa@eagleafrica.co.ke",
+    "outpatientLimit": "10,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "Eagle.jpg" 
+  },
+{
+    "name": "MINET KENYA Insurance",
+    "phoneNumber": "0719044444",
+    "email": "casemanagement@minet.co.ke",
+    "outpatientLimit": "15,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "AON.jpg"
+  }, 
+{
+    "name": "MUA Insurance",
+    "phoneNumber": "0720717580/0780335511",
+    "email": "medical-kenya@mua.co.ke",
+    "outpatientLimit": "15,000",
+    "preAuthorizationServices": ["Speech Therapy", "Surgery", "MRI",  "Vitamin tests", "Pregnancy tests", "HIV test", "Covid-19 tests", "Physiotherapy", "Occupational therapy"],
+    "businessSchemes": [
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": ""
+      },
+      {
+        "name": "",
+        "coPay": "",
+        "billingSystem": "Smart"
+      }
+    ],
+    "icon": "AON.jpg"
+  }, 
+];
+
+function searchInsuranceCompanies() {
+  const searchInput = document.getElementById("searchInput").value.toLowerCase();
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = "";
+
+  insuranceCompaniesData.forEach(company => {
+    if (
+      company.name.toLowerCase().includes(searchInput) ||
+      company.phoneNumber.includes(searchInput)
+    ) {
+      const companyInfo = document.createElement("div");
+      companyInfo.className = "company-info"; // Add a class to the company info container
+      companyInfo.innerHTML = `
+        <h2 class="company-name">${company.name}</h2>
+        <img src="${company.icon}" alt="${company.name} Icon">
+        <p>Phone Number: ${company.phoneNumber}</p>
+        <p>Email: ${company.email}</p>
+        <p>Inpatient Limit: ${company.inpatientLimit}</p>
+        <p>Outpatient Limit: ${company.outpatientLimit}</p>
+        <p>Services Requiring Pre-Authorization:</p>
+        <ul>${company.preAuthorizationServices.map(service => `<li>${service}</li>`).join('')}</ul>
+        <input type="text" class="search-schemes" placeholder="Search Schemes Under this insurance">
+        <div class="scheme-results"></div>
+      `;
+      resultsDiv.appendChild(companyInfo);
+
+      // Attach an event listener for scheme search
+      const schemeInput = companyInfo.querySelector(".search-schemes");
+      const schemeResultsDiv = companyInfo.querySelector(".scheme-results");
+
+      schemeInput.addEventListener("input", () => {
+        searchSchemes(company.businessSchemes, schemeInput.value, schemeResultsDiv);
+      });
+    }
+  });
+}
+
+// Function to search for business schemes
+function searchSchemes(schemes, query, resultsDiv) {
+  resultsDiv.innerHTML = "";
+
+  schemes.forEach(scheme => {
+    if (scheme.name.toLowerCase().includes(query.toLowerCase())) {
+      const schemeInfo = document.createElement("div");
+      schemeInfo.innerHTML = `
+        <h3>${scheme.name}</h3>
+        <p>Co-Pay: ${scheme.coPay}</p>
+        <p>Billing System: ${scheme.billingSystem}</p>
+      `;
+      resultsDiv.appendChild(schemeInfo);
+    }
+  });
+}
